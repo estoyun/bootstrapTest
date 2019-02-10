5bdc849bcef65b707f9c828c1b65f635181e0a37
estoyun <audvuddbs@gmail.com>
2019-02-10 20:24:36 +0900

    초기화

--- bootstrapTest/urls.py
+++ bootstrapTest/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import bootstrap.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', bootstrap.views.home, name="home"),
 ]
